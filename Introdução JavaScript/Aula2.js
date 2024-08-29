a844562d7bb6ff3a64788b72f29829742c88979a
Vinícius Andrade <135062180+ViniciusAndradeCosta@users.noreply.github.com>
2024-08-29 17:29:31 -0300

    Create Aula2.js

---
+++ Introdução JavaScript/Aula2.js
@@ -0,0 +1,21 @@
+//tipos de dados em javascript
+
+//let pode ser atualizado 
+
+let nome = 'Vinicius' //string
+nome = 13 //number
+console.log(typeof nome)
+
+let idade = 20 //number
+console.log(typeof idade)
+
+//em javascript nao tem diferenca se a variavel e inteira ou nao sempre vai ser number
+let salario = 20.5 //number
+console.log(typeof salario)
+
+let aprovado = true //true ou false
+console.log(typeof aprovado)
+
+let curso //undefined
+console.log(typeof curso)
+
