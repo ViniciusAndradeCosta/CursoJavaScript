290d1c6b3e073415f8211ce1e1a7a04e2c20faa3
Vinícius Andrade <135062180+ViniciusAndradeCosta@users.noreply.github.com>
2024-09-01 15:55:12 -0300

    Create Questão3.js

---
+++ Introdução JavaScript/Lista1/Questão3.js
@@ -0,0 +1,12 @@
+//area de um quadrado
+const promptSync = require("prompt-sync")
+
+const input = promptSync()
+
+parseFloat()
+
+const lado = parseFloat(input("Digite o valor do lado do quadrado: "))
+
+const area = lado * lado
+
+console.log("A area do quadro é igual a: ", area)
